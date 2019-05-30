39654e5409246c9dff4e63dda96dbcb66e41e11d
Muammarbanana <muammarfanani2@gmail.com>
2019-05-30 15:13:22 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+    require __DIR__ . '/vendor/autoload.php';
+     
+    use \LINE\LINEBot;
+    use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+    use \LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
+    use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+    use \LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
+    use \LINE\LINEBot\SignatureValidator as SignatureValidator;
+     
+    // set false for production
+    $pass_signature = true;
+     
+    // set LINE channel_access_token and channel_secret
+    $channel_access_token = "";
+    $channel_secret = "";
+     
+    // inisiasi objek bot
+    $httpClient = new CurlHTTPClient($channel_access_token);
+    $bot = new LINEBot($httpClient, ['channelSecret' => $channel_secret]);
+     
+    $configs =  [
+        'settings' => ['displayErrorDetails' => true],
+    ];
+    $app = new Slim\App($configs);
+     
+    // buat route untuk url homepage
+    $app->get('/', function($req, $res)
+    {
+      echo "Welcome at Slim Framework";
+    });
+     
+    // buat route untuk webhook
+    $app->post('/webhook', function ($request, $response) use ($bot, $pass_signature)
+    {
+        // get request body and line signature header
+        $body        = file_get_contents('php://input');
+        $signature = isset($_SERVER['HTTP_X_LINE_SIGNATURE']) ? $_SERVER['HTTP_X_LINE_SIGNATURE'] : '';
+     
+        // log body and signature
+        file_put_contents('php://stderr', 'Body: '.$body);
+     
+        if($pass_signature === false)
+        {
+            // is LINE_SIGNATURE exists in request header?
+            if(empty($signature)){
+                return $response->withStatus(400, 'Signature not set');
+            }
+     
+            // is this request comes from LINE?
+            if(! SignatureValidator::validateSignature($body, $channel_secret, $signature)){
+                return $response->withStatus(400, 'Invalid signature');
+            }
+        }
+     
+        // kode aplikasi nanti disini
+     
+    });
+     
+    $app->run();
+?>
