ae1c626e71be0260ba230853c797c25f64a63e67
Muammarbanana <muammarfanani2@gmail.com>
2019-06-03 16:55:19 +0700

    goback

--- index.php
+++ index.php
@@ -120,7 +120,7 @@
                             }
                             $newflex = json_encode($data_carousel);
                             file_put_contents("carousel_hasil_search2.json",$newflex);
-                            $flex_template2 = file_get_contents("hehe.json");
+                            $flex_template2 = file_get_contents("carousel_hasil_search2.json");
                             $result = $httpClient->post(LINEBot::DEFAULT_ENDPOINT_BASE . '/v2/bot/message/reply', [
                                 'replyToken' => $event['replyToken'],
                                 'messages'   => [
